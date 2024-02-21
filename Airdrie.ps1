7d1d4dc9d0772898bcb61a3e279614f4f0a40e1e
bastardsandcheats <wason@outlook.com>
2024-02-21 18:33:31 +0000

    Update Airdrie.ps1

--- Airdrie.ps1
+++ Airdrie.ps1
@@ -28,7 +28,7 @@ $iniContent = @"
 [PATH]
 ROOTPATH=z:\Lab Reports\System\Installation\
 "@
-$iniContent | Out-File -FilePath $labInfoPath -Encoding ASCII
+$iniContent | Out-File -FilePath $labInfoPath -Encoding UTF-8
 
 # Step 3: Create a shortcut to Excel with arguments
 $shortcutPath = [System.Environment]::GetFolderPath('CommonDesktopDirectory') + "\Lab Reports on prem.lnk"
