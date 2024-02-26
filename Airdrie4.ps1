38ddf2b04cd330fe2748866c8e51d4439c2338e1
bastardsandcheats <wason@outlook.com>
2024-02-26 09:56:10 +0000

    Update Airdrie4.ps1

--- Airdrie4.ps1
+++ Airdrie4.ps1
@@ -44,3 +44,15 @@ $Shortcut.IconLocation = "C:\Windows\System32\imageres.dll,99"
 $Shortcut.Save()
 
 Write-Host "Operation completed successfully."
+
+# Define the URL of the file to download
+$url = "https://raw.githubusercontent.com/bastardsandcheats/TempLR/main/MapLabreportsAirdrie.bat"
+
+# Define the destination path (Public Desktop)
+$publicDesktop = [System.Environment]::GetFolderPath('CommonDesktopDirectory')
+$destination = Join-Path -Path $publicDesktop -ChildPath "MapLabreportsAirdrie.bat"
+
+# Use Invoke-WebRequest to download the file
+Invoke-WebRequest -Uri $url -OutFile $destination
+
+Write-Host "File downloaded to $destination"
